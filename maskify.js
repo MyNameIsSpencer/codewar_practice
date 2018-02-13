2fe3220c623c9645e42b60eb468f5e7f03502006
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-13 13:23:52 -0500

    finish maskify

--- maskify.js
+++ maskify.js
@@ -1,12 +1,27 @@
 // return masked string
 function maskify(cc) {
-
+  var hashes = '';
+  for (i = 0; i < cc.slice(0, -4).length; i ++) {
+    hashes += '#';
+  }
+  return (hashes += cc.slice(-4));
 }
 
 
 
+var heeeyyy = "HHhhhhheeeEEEEEeeeeyy";
+const numb = heeeyyy.slice(0, -4).length;
+const pracfer = `${"#" * numb}`;
+const firstfer = heeeyyy.slice(0, -4);
+var hashes = '';
+for (i = 0; i < heeeyyy.slice(0, -4).length; i ++) {
+  hashes += '#';
+}
 
 
+console.log(maskify(heeeyyy));
+console.log(numb);
+console.log(hashes);
 //
 //
 // Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
