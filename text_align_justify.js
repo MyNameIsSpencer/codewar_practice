78d3ea1bc8f8a5dbc0fb7a9335fb59ae4f52d29a
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-13 18:58:58 -0500

    include new line at len spaces

--- text_align_justify.js
+++ text_align_justify.js
@@ -3,16 +3,35 @@ var warble = 'Today has been a lovely day of psychosomatic liberation. Tomorrow,
 function justify(str, len) {
   var splitter = str.split(" ");
   var counter = 0;
-  var newStr = null;
+  var newStr = "";
+  var line = [];
   for (i = 0; i < splitter.length; i ++) {
-    counter += 
-    newStr += splitter[i] + ' ';
-    if (counter % len === 0) {
-      newStr += '\n'
+    counter += splitter[i].length + 1
+    if (counter > (len - 1)) {
+      line.pop();
+      let short = counter - splitter[i].length - 2
+      // line.push('\n');
+      for (j = 0; j < line.length; j ++) {
+        newStr += line[j]
+      }
+      newStr += '\n';
+      line = [];
+      counter = 0;
+      line.push(splitter[i]);
+      line.push(" ");
+    } else if (i === splitter.length) {
+      line.push(splitter[i]);
+      for (j = 0; j < line.length; j ++) {
+        newStr += line[j]
+      }
+    } else {
+      line.push(splitter[i]);
+      line.push(" ");
     }
+
   }
   return newStr
-};
+}
 
 
 console.log(justify(warble, 20));
