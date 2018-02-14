8ec8eb7d64f46336f1a9a6fa62c85e1c87d9fb34
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-13 23:14:28 -0500

    text align almost working

--- text_align_justify.js
+++ text_align_justify.js
@@ -1,4 +1,4 @@
-var warble = 'Today has been a lovely day of psychosomatic liberation. Tomorrow, who knows? I do not. Each new day is another confrontation if our inevitable end, the best we can hope for is a long life with good memories.';
+var warble = 'Today has been a lovely day of psychosomatic liberation. Tomorrow, who knows? I do not. Each new day is another confrontation of our inevitable end, the best we can hope for is a long life with good memories.';
 
 function justify(str, len) {
   var splitter = str.split(" ");
@@ -6,11 +6,20 @@ function justify(str, len) {
   var newStr = "";
   var line = [];
   for (i = 0; i < splitter.length; i ++) {
-    counter += splitter[i].length + 1
-    if (counter > (len - 1)) {
-      line.pop();
-      let short = counter - splitter[i].length - 2
-      // line.push('\n');
+    counter += splitter[i].length;
+    if (counter > len) {
+      var short = (len - counter + splitter[i].length + 1 + line.length - 1);
+      var divider = Math.floor(short / (line.length - 1));
+      var remains = short % (line.length - 1);
+      for (k = 0; k < (line.length - 1); k ++) {
+        for (a = 0; a < divider; a ++) {
+          line[k] += " ";
+        }
+      }
+      for (b = 0; b < remains; b++) {
+        line[b] += " ";
+      }
+
       for (j = 0; j < line.length; j ++) {
         newStr += line[j]
       }
@@ -18,20 +27,23 @@ function justify(str, len) {
       line = [];
       counter = 0;
       line.push(splitter[i]);
-      line.push(" ");
-    } else if (i === splitter.length) {
+      counter += (splitter[i].length + 1);
+    } else if (i === (splitter.length - 1)  && len != counter) {
       line.push(splitter[i]);
-      for (j = 0; j < line.length; j ++) {
-        newStr += line[j]
+      for (j = 0; j < (line.length - 1); j ++) {
+        newStr += line[j];
+        newStr += ' ';
       }
+      newStr += line[line.length -1];
+    } else if (i === (splitter.length - 1)  && len === counter) {
     } else {
       line.push(splitter[i]);
-      line.push(" ");
+      counter += 1
     }
 
   }
   return newStr
-}
+};
 
 
 console.log(justify(warble, 20));
